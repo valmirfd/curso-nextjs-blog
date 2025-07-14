f1db8c5ae7b85ac811ed93ff18e7bd1f6967e5b0
Valmir Dornas <fernandocursos2021@gmail.com>
2025-07-14 14:34:56 -0300

    Trabalhando com css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,46 @@ import { Container } from '@/components/Container';
 import { Header } from '@/components/Header';
 import { PostsList } from '@/components/PostsList';
 import { SpinLoader } from '@/components/SpinLoader';
+import Image from 'next/image';
+import Link from 'next/link';
 import { Suspense } from 'react';
 
 export default async function HomePage() {
   return (
     <Container>
       <Header />
+
+      <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
+        <Link className='w-full h-full overflow-hidden rounded-xl' href='#'>
+          <Image
+            className='w-full h-full object-cover object-center group-hover:scale-105 transition'
+            src='/images/bryen_0.png'
+            width={1200}
+            height={720}
+            alt='Título do post'
+            priority
+          />
+        </Link>
+        <div className='flex flex-col gap-4 sm:justify-center'>
+          <time
+            className='text-slate-600 block text-sm/tight'
+            dateTime='2025-07-14'
+          >
+            14/07/2025 13:52
+          </time>
+
+          <h1 className='text-2xl/tight font-extrabold sm:text-4xl'>
+            <Link href='#'>Lorem ipsum, dolor sit amet consectetur</Link>
+          </h1>
+
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Asperiores, similique hic rem officiis delectus non ab facere cumque
+            aspernatur in nobis necessitatibus numquam ipsa deserunt, assumenda,
+            corrupti earum nostrum error!
+          </p>
+        </div>
+      </section>
 
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
