e0c5fbcc5cb98c3c94c6a6a2d11adacaba2e7daf
Valmir Dornas <fernandocursos2021@gmail.com>
2025-07-15 21:06:05 -0300

    PostList

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import { Container } from '@/components/Container';
 import { Header } from '@/components/Header';
+import { PostCoverImage } from '@/components/PostCoverImage';
 import { PostHeading } from '@/components/PostHeading';
 import { PostsList } from '@/components/PostsList';
 import { SpinLoader } from '@/components/SpinLoader';
-import Image from 'next/image';
-import Link from 'next/link';
+
 import { Suspense } from 'react';
 
 export default async function HomePage() {
@@ -13,16 +13,18 @@ export default async function HomePage() {
       <Header />
 
       <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
-        <Link className='w-full h-full overflow-hidden rounded-xl' href='#'>
-          <Image
-            className='w-full h-full object-cover object-center group-hover:scale-105 transition'
-            src='/images/bryen_0.png'
-            width={1200}
-            height={720}
-            alt='Título do post'
-            priority
-          />
-        </Link>
+        <PostCoverImage
+          linkProps={{
+            href: '/post/jesus',
+          }}
+          imageProps={{
+            width: 1200,
+            height: 720,
+            src: '/images/bryen_6.png',
+            alt: 'Imagem principal',
+            priority: true,
+          }}
+        />
         <div className='flex flex-col gap-4 sm:justify-center'>
           <time
             className='text-slate-600 block text-sm/tight'
